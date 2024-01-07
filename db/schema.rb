0ea4305a4d04e7862c1edb22f8d0dd205d846c48
yuriririn <80314978+yuriririn@users.noreply.github.com>
2024-01-07 16:29:09 +0900

    技のdb作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_01_06_052534) do
+ActiveRecord::Schema[7.0].define(version: 2024_01_07_070601) do
+  create_table "skills", force: :cascade do |t|
+    t.string "name"
+    t.text "koka"
+    t.integer "attack_point"
+    t.string "symbol"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "supports", force: :cascade do |t|
     t.string "name"
     t.text "koka"
