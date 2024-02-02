f6f6136d48c3125892941213defc65f92d4de896
yuriririn <80314978+yuriririn@users.noreply.github.com>
2024-02-02 21:01:56 +0900

    外部キーの設定の記述

--- app/models/card_in_deck.rb
+++ app/models/card_in_deck.rb
@@ -1,2 +1,8 @@
 class CardInDeck < ApplicationRecord
+  belongs_to :pokemon
+  belongs_to :item
+  belongs_to :support
+  belongs_to :pokemon_no_item
+  belongs_to :stajiamu
+  belongs_to :energy
 end
