7976f8285a4fbcf932cb2e86b522ca83d0b2276b
yuriririn <80314978+yuriririn@users.noreply.github.com>
2024-02-02 21:37:42 +0900

    外部キー設定
    ・情報登録
    ・nilの設定

--- app/models/card_in_deck.rb
+++ app/models/card_in_deck.rb
@@ -1,9 +1,9 @@
 class CardInDeck < ApplicationRecord
   belongs_to :deck
-  belongs_to :pokemon
-  belongs_to :item
-  belongs_to :support
-  belongs_to :pokemon_no_item
-  belongs_to :stajiamu
-  belongs_to :energy
+  belongs_to :pokemon, optional: true
+  belongs_to :item, optional: true
+  belongs_to :support, optional: true
+  belongs_to :pokemon_no_item, optional: true
+  belongs_to :stajiamu, optional: true
+  belongs_to :energy, optional: true
 end
