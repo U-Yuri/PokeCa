bd074a88624d485e91345275207d364702ae8841
ktomomi0333 <108171290+ktomomi0333@users.noreply.github.com>
2024-02-03 18:11:02 +0900

    apps/models/skill.rbで外部キーの設定を変更しました。

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,4 +1,4 @@
 class Skill < ApplicationRecord
-  has_many :pokemons, foreign_key: :skill1_id
-  has_many :pokemons, foreign_key: :skill2_id
+  has_many :pokemons1, class_name: 'Pokemon', foreign_key: :skill1_id
+  has_many :pokemons2, class_name: 'Pokemon', foreign_key: :skill2_id
 end
