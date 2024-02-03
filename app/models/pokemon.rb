cde2c07e096a4f2b4a13d9f95b5cf1e1c5f0171a
ktomomi0333 <108171290+ktomomi0333@users.noreply.github.com>
2024-02-03 13:08:16 +0900

    pokemonsテーブルの外部キー設定を修正しました。

--- app/models/pokemon.rb
+++ app/models/pokemon.rb
@@ -1,5 +1,5 @@
 class Pokemon < ApplicationRecord
-  belongs_to :skill, class_name: 'Skill', foreign_key: :skill1_id
-  belongs_to :skill, class_name: 'Skill', foreign_key: :skill2_id
-  belongs_to :skill, class_name: 'Tokusei', foreign_key: :tokusei_id
+  belongs_to :skill1, class_name: 'Skill', foreign_key: :skill1_id
+  belongs_to :skill2, class_name: 'Skill', foreign_key: :skill2_id, optional: true
+  belongs_to :tokusei
 end
